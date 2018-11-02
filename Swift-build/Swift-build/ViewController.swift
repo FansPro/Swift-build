71cb7a84f15a8c868c46f634eae9d79f9ac415dd
FansX <pengfan@9999ax.com>
2018-11-02 16:52:26 +0800

    init

---
+++ Swift-build/Swift-build/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-build
+//
+//  Created by 彭帆 on 2018/11/2.
+//  Copyright © 2018年 FansX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
